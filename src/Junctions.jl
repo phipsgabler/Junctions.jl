2a764ab17079cfba8c14b58dc816cc6933fecd06
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-12-27 20:54:31 +0100

    Replace nested continuations by if-else chain, and make things work. Also: one-argument broadcasting.

--- src/Junctions.jl
+++ src/Junctions.jl
@@ -1,6 +1,6 @@
 module Junctions
 
-import Base: convert, getindex, similar
+import Base: convert, getindex, reduce
 import Base.Broadcast
 
 export @all, @any,
@@ -22,15 +22,31 @@ end
 struct And end
 struct Or end
 
-eigenstates(j::Junction, ::Type{T} = Any) where T = j.reducer(T[], (l, r) -> push!(l, r[]))
-
-convert(::Type{Bool}, j::Junction{And}) = j.reducer(true, (l, r) -> l && r[])
-convert(::Type{Bool}, j::Junction{Or}) = j.reducer(false, (l, r) -> l || r[])
-
 
 function construct_junction(typ, values)
-    comps = foldl((l, r) -> :($l ∘ Shift(() -> $r)), esc.(values), init = :i)
-    :(Junction{$typ}((i, ∘) -> $comps))
+    # comps = foldl((current, rest) -> :($current ∘ (() -> $rest)), esc.(values), init = :i)
+    # comps = foldr((current, rest) -> :($current ∘ (() -> $rest)), esc.(reverse(values)), init = :i)
+    # :(Junction{$typ}((init, op, cont) -> $comps))
+
+    expr = :(nothing)
+    for v in Iterators.reverse(values)
+        expr = quote
+            let v = $v
+                acc = op(acc, v)
+                if cont(v)
+                    $expr
+                end
+            end
+        end
+    end
+
+    expr = quote
+        acc = init
+        $expr
+        return acc
+    end
+
+    :(Junction{$typ}((init, op, cont) -> $expr))
 end
 
 macro all(values...)
@@ -42,9 +58,27 @@ macro any(values...)
 end
 
 
+reduce(op, j::Junction, init) = j.reducer(init, op, _ -> true)
+
+eigenstates(j::Junction, ::Type{T} = Any) where T = reduce(push!, j, T[])
+
+convert(::Type{Bool}, j::Junction{And}) = j.reducer(true, &, identity)
+convert(::Type{Bool}, j::Junction{Or}) = j.reducer(false, |, !)
+
+
 # broadcasting
-BroadcastStyle(::Type{<:Junction}) = Broadcast.Style{Junction}()
-# similar(bc::Broadcasted{Broadcast.Style{Junction}}, ::Type{Elype}) where Eltype =
-    # Junction{Eltype}((i, ))
+struct JunctionStyle <: Broadcast.BroadcastStyle end
+Base.BroadcastStyle(::Type{<:Junction}) = JunctionStyle()
+Broadcast.BroadcastStyle(s::JunctionStyle, ::Broadcast.BroadcastStyle) = s
+
+Broadcast.broadcastable(x::Junction) = x
+
+function Broadcast.broadcasted(::JunctionStyle, f, arg1::Junction{T}) where T
+    function reducer(init, op, cont)
+        arg1.reducer(init, (acc, v) -> op(acc, f(v)), v -> cont(f(v)))
+    end
+    Junction{T}(reducer)
+end
+
 
 end # module
